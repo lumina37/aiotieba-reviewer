117e90c8ec52f9d228ca3205ebb7b2ea78a49650
Starry-OvO <starry.qvq@gmail.com>
2023-05-03 20:44:37 +0800

    docs: update readme

--- examples/reviewer_example.py
+++ examples/reviewer_example.py
@@ -5,7 +5,6 @@ from typing import List, Optional, Tuple
 
 import aiotieba as tb
 from aiotieba.api.get_homepage import Thread_home, UserInfo_home
-from aiotieba.api.get_posts import UserInfo_pt
 from aiotieba.typing import Comment, Post, Thread
 from cacheout import Cache
 
@@ -39,7 +38,7 @@ async def check_thread(thread: Thread) -> Optional[Punish]:
         if re.search(r'氵|\+3|➕3|加三|加3|经验|jy', thread.text):
             return Punish(thread, Ops.DELETE, 1, note="单开水楼")
 
-    user: UserInfo_pt = thread.user
+    user = thread.user
 
     # 作图广告
     for at in thread.contents.ats:
